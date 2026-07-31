d6f399ebd5014ad3dc370245ea94fb879ca9c1fd
LeSama <1506536196@qq.com>
2026-08-01 01:51:54 +0800

    Spring AOP 底层是基于动态代理技术实现， 程序运行时会生成代理对象执行对于逻辑

---
+++ java-web-codes/web-project-02/lesama-dept-management/src/main/java/com/lesama/aop/RecordTimeAspect.java
@@ -0,0 +1,22 @@
+package com.lesama.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class RecordTimeAspect {
+    @Around("execution(* com.lesama.service.impl.*.*(..))")
+    public Object recordTime(ProceedingJoinPoint pjp) throws Throwable {
+        log.info("记录时间");
+        long start = System.currentTimeMillis();
+        Object result = pjp.proceed();
+        long end = System.currentTimeMillis();
+        log.info("{}方法耗时：{}ms", pjp.getSignature(), end - start);
+        return result;
+    }
+}
