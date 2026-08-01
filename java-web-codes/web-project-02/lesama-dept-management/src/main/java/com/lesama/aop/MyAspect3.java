3dea2d14f919cfdc0c567c08f6a804f3f6759563
LeSama <1506536196@qq.com>
2026-08-01 22:47:53 +0800

    多个切面类通知方法的执行顺序，before按照字母排序从前到后，after相反，可以使用Order注解来控制执行顺序

---
+++ java-web-codes/web-project-02/lesama-dept-management/src/main/java/com/lesama/aop/MyAspect3.java
@@ -0,0 +1,26 @@
+package com.lesama.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+@Order(2)
+// 演示切面类的五种通知类型
+public class MyAspect3 {
+
+    @Before("execution(* com.lesama.service.impl.*.*(..))")
+    public void before() {
+        log.info("MyAspect3 before");
+    }
+
+    @After("execution(* com.lesama.service.impl.*.*(..))")
+    public void after() {
+        log.info("MyAspect3 after");
+    }
+
+}
