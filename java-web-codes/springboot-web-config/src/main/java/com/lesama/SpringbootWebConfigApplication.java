7b32dad1a742a3c7eb73cdd4f409a20854b71ef1
LeSama <1506536196@qq.com>
2026-08-05 00:24:19 +0800

    第三方Bean对象使用Bean注解交给IOC容器管理

--- java-web-codes/springboot-web-config/src/main/java/com/lesama/SpringbootWebConfigApplication.java
+++ java-web-codes/springboot-web-config/src/main/java/com/lesama/SpringbootWebConfigApplication.java
@@ -1,11 +1,19 @@
 package com.lesama;
 
+import com.lesama.utils.AliyunOSSOperator;
+import com.lesama.utils.AliyunOSSProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
 public class SpringbootWebConfigApplication {
+
+    @Bean
+    public AliyunOSSOperator aliyunOSSOperator(AliyunOSSProperties aliyunOSSProperties) {
+        return new AliyunOSSOperator(aliyunOSSProperties);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootWebConfigApplication.class, args);
--- java-web-codes/springboot-web-config/src/test/java/com/lesama/SpringbootWebTests.java
+++ java-web-codes/springboot-web-config/src/test/java/com/lesama/SpringbootWebTests.java
@@ -1,16 +1,24 @@
 package com.lesama;
 
+import cn.hutool.core.io.FileUtil;
 import com.lesama.controller.DeptController;
+import com.lesama.utils.AliyunOSSOperator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+
+import java.io.File;
+import java.util.logging.Filter;
 
 @SpringBootTest
 class SpringbootWebTests {
 
     @Autowired
     private ApplicationContext applicationContext;
+
+    @Autowired
+    private AliyunOSSOperator aliyunOSSOperator;
 
     @Test
     void testSingleTon() {
@@ -20,5 +28,12 @@ class SpringbootWebTests {
             DeptController deptController = applicationContext.getBean(DeptController.class);
             System.out.println(deptController);
         }
+    }
+
+    @Test
+    void testUpload() throws Exception {
+        File file = new File("C:\\Users\\Administrator\\Pictures\\2\\daniya.png");
+        byte[] bytes = FileUtil.readBytes(file);
+        aliyunOSSOperator.upload(bytes, "daniya.png");
     }
 }
