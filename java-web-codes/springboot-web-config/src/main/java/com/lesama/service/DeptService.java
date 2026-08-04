2ea7a500dfe3f35aecd11e054a953d170e973499
LeSama <1506536196@qq.com>
2026-08-04 21:28:49 +0800

    Spring Boot 配置优先级： 命令行参数 > Java系统属性 > application.properties > application.yml > application.yaml

---
+++ java-web-codes/springboot-web-config/src/main/java/com/lesama/service/DeptService.java
@@ -0,0 +1,32 @@
+package com.lesama.service;
+
+import com.lesama.pojo.Dept;
+
+import java.util.List;
+
+public interface DeptService {
+    /**
+     * 查询全部部门
+     */
+    public List<Dept> findAll();
+
+    /**
+     * 根据ID删除部门
+     */
+    void delete(Integer id);
+
+    /**
+     * 新增部门
+     */
+    void save(Dept dept);
+
+    /**
+     * 根据ID查询部门
+     */
+    Dept getInfo(Integer id);
+
+    /**
+     * 更新部门
+     */
+    void update(Dept dept);
+}
---
+++ java-web-codes/springboot-web-config/src/test/java/com/lesama/SpringbootWebTests.java
@@ -0,0 +1,8 @@
+package com.lesama;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebTests {
+
+}
