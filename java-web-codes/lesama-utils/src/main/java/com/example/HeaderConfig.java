dcbfad071db0d20defec814afb9539d3b88720ec
LeSama <1506536196@qq.com>
2026-08-06 00:33:25 +0800

    Conditionalxxx注解： 当环境中满足特定条件时，才会创建Bean对象

--- java-web-codes/lesama-utils/src/main/java/com/example/HeaderConfig.java
+++ java-web-codes/lesama-utils/src/main/java/com/example/HeaderConfig.java
@@ -1,5 +1,8 @@
 package com.example;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,11 +10,14 @@ import org.springframework.context.annotation.Configuration;
 public class HeaderConfig {
 
     @Bean
+    @ConditionalOnClass(name = "io.jsonwebtoken.Jwts") // 只有io.jsonwebtoken.Jwts类存在时，才会创建HeaderParser对象
     public HeaderParser headerParser(){
         return new HeaderParser();
     }
 
     @Bean
+//    @ConditionalOnMissingBean // 只有HeaderGenerator类不存在时，才会创建HeaderGenerator对象
+    @ConditionalOnProperty(name = "name", havingValue = "lesama") // 只有name属性的值为lesama时，才会创建HeaderGenerator对象
     public HeaderGenerator headerGenerator(){
         return new HeaderGenerator();
     }
