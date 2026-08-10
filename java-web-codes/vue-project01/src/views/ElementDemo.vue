dc0ca8458a2a0842918a9a4f49571410c11e635b
LeSama <1506536196@qq.com>
2026-08-11 00:06:08 +0800

    ElementPlus入门，表格组件

--- java-web-codes/vue-project01/src/views/ElementDemo.vue
+++ java-web-codes/vue-project01/src/views/ElementDemo.vue
@@ -1,5 +1,10 @@
 <script setup>
-
+const tableData = [
+  {date: '2016-05-03', name: 'Tom', address: 'No. 189, Grove St, Los Angeles'},
+  {date: '2016-05-02', name: 'Tom', address: 'No. 189, Grove St, Los Angeles'},
+  {date: '2016-05-04', name: 'Tom', address: 'No. 189, Grove St, Los Angeles'},
+  {date: '2016-05-01', name: 'Tom', address: 'No. 189, Grove St, Los Angeles'}
+]
 </script>
 
 <template>
@@ -11,6 +16,15 @@
     <el-button type="warning">Warning</el-button>
     <el-button type="danger">Danger</el-button>
   </el-row>
+
+  <br>
+
+  <!-- Table表格 -->
+  <el-table :data="tableData" border style="width: 100%">
+    <el-table-column prop="date" label="Date" width="180" />
+    <el-table-column prop="name" label="Name" width="180" />
+    <el-table-column prop="address" label="Address" />
+  </el-table>
 </template>
 
 <style scoped>
