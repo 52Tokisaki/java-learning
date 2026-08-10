5d51bd359453954a07e894ec4e2993fa9c6161e1
LeSama <1506536196@qq.com>
2026-08-11 00:10:46 +0800

    ElementPlus入门，对话框组件

--- java-web-codes/vue-project01/src/views/ElementDemo.vue
+++ java-web-codes/vue-project01/src/views/ElementDemo.vue
@@ -19,6 +19,8 @@ const handleSizeChange = (val) => {
 const handleCurrentChange = (val) => {
   console.log(`current page: ${val}`);
 };
+
+const dialogTableVisible = ref(false);
 </script>
 
 <template>
@@ -48,7 +50,20 @@ const handleCurrentChange = (val) => {
       :total="400"
       @size-change="handleSizeChange"
       @current-change="handleCurrentChange"
+      class="mb-4"
   />
+
+  <el-button @click="dialogTableVisible = true">
+    打开对话框
+  </el-button>
+
+  <el-dialog v-model="dialogTableVisible" title="Shipping address">
+    <el-table :data="tableData">
+      <el-table-column property="date" label="Date" width="150" />
+      <el-table-column property="name" label="Name" width="200" />
+      <el-table-column property="address" label="Address" />
+    </el-table>
+  </el-dialog>
 </template>
 
 <style scoped>
