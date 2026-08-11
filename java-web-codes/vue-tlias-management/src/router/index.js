a366ae908dc263087c38c87e6cfbdcce2864b229
LeSama <1506536196@qq.com>
2026-08-11 22:43:18 +0800

    使用嵌套路由将路由结构完善

--- java-web-codes/vue-tlias-management/src/router/index.js
+++ java-web-codes/vue-tlias-management/src/router/index.js
@@ -9,49 +9,56 @@ const router = createRouter({
     //   component: HomeView
     // },
     {
-      path: '/index',
-      name: 'index',
-      component: () => import('@/views/index/index.vue')
-    },
-    {
-      path: '/clazz',
-      name: 'clazz',
-      component: () => import('@/views/clazz/index.vue')
-    },
-    {
-      path: '/dept',
-      name: 'dept',
-      component: () => import('@/views/dept/index.vue')
-    },
-    {
-      path: '/emp',
-      name: 'emp',
-      component: () => import('@/views/emp/index.vue')
-    },
-    {
-      path: '/log',
-      name: 'log',
-      component: () => import('@/views/log/index.vue')
+      path: '/',
+      component: () => import('@/views/layout/index.vue'),
+      redirect: '/index',
+      children: [
+        {
+          path: 'index',
+          name: 'index',
+          component: () => import('@/views/index/index.vue')
+        },
+        {
+          path: 'clazz',
+          name: 'clazz',
+          component: () => import('@/views/clazz/index.vue')
+        },
+        {
+          path: 'dept',
+          name: 'dept',
+          component: () => import('@/views/dept/index.vue')
+        },
+        {
+          path: 'emp',
+          name: 'emp',
+          component: () => import('@/views/emp/index.vue')
+        },
+        {
+          path: 'log',
+          name: 'log',
+          component: () => import('@/views/log/index.vue')
+        },
+        {
+          path: 'empReport',
+          name: 'empReport',
+          component: () => import('@/views/report/emp/index.vue')
+        },
+        {
+          path: 'stuReport',
+          name: 'stuReport',
+          component: () => import('@/views/report/stu/index.vue')
+        },
+        {
+          path: 'stu',
+          name: 'stu',
+          component: () => import('@/views/stu/index.vue')
+        },
+      ]
     },
     {
       path: '/login',
       name: 'login',
       component: () => import('@/views/login/index.vue')
-    },
-    {
-      path: '/report/emp',
-      name: 'empReport',
-      component: () => import('@/views/report/emp/index.vue')
-    },
-    {
-      path: '/report/stu',
-      name: 'stuReport',
-      component: () => import('@/views/report/stu/index.vue')
-    },
-    {
-      path: '/stu',
-      name: 'stu',
-      component: () => import('@/views/stu/index.vue')
     },
   ]
 })
