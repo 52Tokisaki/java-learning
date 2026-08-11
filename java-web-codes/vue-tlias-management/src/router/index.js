a3d3691b0b358ffa0b656d161d2f5db25b772bc0
LeSama <1506536196@qq.com>
2026-08-11 22:26:31 +0800

    添加各个组件的路由模块

--- java-web-codes/vue-tlias-management/src/router/index.js
+++ java-web-codes/vue-tlias-management/src/router/index.js
@@ -8,11 +8,51 @@ const router = createRouter({
     //   name: 'home',
     //   component: HomeView
     // },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   component: () => import('../views/AboutView.vue')
-    // }
+    {
+      path: '/index',
+      name: 'index',
+      component: () => import('@/views/index/index.vue')
+    },
+    {
+      path: '/clazz',
+      name: 'clazz',
+      component: () => import('@/views/clazz/index.vue')
+    },
+    {
+      path: '/dept',
+      name: 'dept',
+      component: () => import('@/views/dept/index.vue')
+    },
+    {
+      path: '/emp',
+      name: 'emp',
+      component: () => import('@/views/emp/index.vue')
+    },
+    {
+      path: '/log',
+      name: 'log',
+      component: () => import('@/views/log/index.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/login/index.vue')
+    },
+    {
+      path: '/report/emp',
+      name: 'empReport',
+      component: () => import('@/views/report/emp/index.vue')
+    },
+    {
+      path: '/report/stu',
+      name: 'stuReport',
+      component: () => import('@/views/report/stu/index.vue')
+    },
+    {
+      path: '/stu',
+      name: 'stu',
+      component: () => import('@/views/stu/index.vue')
+    },
   ]
 })
 
