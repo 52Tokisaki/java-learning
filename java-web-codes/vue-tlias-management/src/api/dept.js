c34c8b33ae95acc581220fd141aa5c546e9301e9
LeSama <1506536196@qq.com>
2026-08-12 20:11:36 +0800

    新增部门实现

--- java-web-codes/vue-tlias-management/src/api/dept.js
+++ java-web-codes/vue-tlias-management/src/api/dept.js
@@ -1,3 +1,11 @@
 import request from '@/utils/request';
 
 export const getDeptList = () => request.get('/depts');
+
+export const insertDept = (dept) => request.post('/depts', dept);
+
+export const updateDept = (id, dept) => request.put(`/depts/${id}`, dept);
+
+export const getDeptById = (id) => request.get(`depts/${id}`);
+
+export const deleteDeptById = (id) => request.delete(`depts/${id}`);
--- java-web-codes/vue-tlias-management/src/views/dept/index.vue
+++ java-web-codes/vue-tlias-management/src/views/dept/index.vue
@@ -1,7 +1,8 @@
 <script setup>
 
 import { onMounted, ref } from "vue";
-import { getDeptList } from "@/api/dept";
+import { getDeptList, insertDept } from "@/api/dept";
+import { ElMessage } from "element-plus";
 
 const deptList = ref([]);
 
@@ -18,6 +19,46 @@ const getTableData = async () => {
   deptList.value = result.data;
 };
 
+const showDialog = ref(false);
+const dialogTitle = ref('');
+const deptForm = ref({ name: '' });
+const formRef = ref();
+const rules = ref({
+  name: [
+    { required: true, message: '请输入部门名称', trigger: 'blur' },
+    { min: 2, max: 10, message: '部门名称长度为2-10位字符', trigger: 'blur' },
+  ],
+});
+
+const addDept = () => {
+  dialogTitle.value = '新增部门';
+  showDialog.value = true;
+};
+
+const onSubmit = () => {
+  console.log(deptForm.value);
+
+  if (!formRef.value) {
+    return;
+  }
+
+  formRef.value.validate(async valid => {
+    if (!valid) {
+      ElMessage.error('表单校验失败');
+      return;
+    }
+    if (dialogTitle.value === '新增部门') {
+      const result = await insertDept(deptForm.value);
+      if (result.code === 1) {
+        ElMessage.success('新增部门成功');
+      } else {
+        ElMessage.error('新增部门失败');
+      }
+    }
+    showDialog.value = false;
+  });
+};
+
 onMounted(() => {
   getTableData();
 });
@@ -25,21 +66,50 @@ onMounted(() => {
 
 <template>
   <h1>部门管理</h1>
-  <div style="margin: 20px 0"><el-button type="primary">新增部门</el-button></div>
+  <div style="margin: 20px 0">
+    <el-button type="primary" @click="addDept">新增部门</el-button>
+  </div>
 
-  <el-table :data="deptList" border style="width: 100%;">
-    <el-table-column type="index" label="序号" width="100" align="center"/>
-    <el-table-column prop="name" label="部门名称" width="300" align="center"/>
-    <el-table-column prop="updateTime" label="最后修改时间" width="300" align="center"/>
+  <el-table :data="deptList" border style="width: 100%">
+    <el-table-column type="index" label="序号" width="100" align="center" />
+    <el-table-column prop="name" label="部门名称" width="300" align="center" />
+    <el-table-column
+      prop="updateTime"
+      label="最后修改时间"
+      width="300"
+      align="center"
+    />
     <el-table-column fixed="right" label="操作" align="center">
       <template #default="scope">
-        <el-button size="small" @click="handleEdit(scope.row.id)">修改</el-button>
-        <el-button size="small" type="danger" @click="handleDelete(scope.row.id)">删除</el-button>
+        <el-button size="small" @click="handleEdit(scope.row.id)"
+        >修改</el-button
+        >
+        <el-button
+          size="small"
+          type="danger"
+          @click="handleDelete(scope.row.id)"
+        >删除</el-button
+        >
       </template>
     </el-table-column>
   </el-table>
+
+  <el-dialog v-model="showDialog" :title="dialogTitle">
+    <el-form :model="deptForm" :rules="rules" ref="formRef">
+      <el-form-item label="部门名称" prop="name">
+        <el-input v-model="deptForm.name" />
+      </el-form-item>
+    </el-form>
+
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button @click="showDialog = false">取消</el-button>
+        <el-button type="primary" @click="onSubmit">
+          确认
+        </el-button>
+      </div>
+    </template>
+  </el-dialog>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
