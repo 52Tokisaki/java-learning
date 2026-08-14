aeaf81a04025fbd66bc2173b06f7979ceb17d638
LeSama <1506536196@qq.com>
2026-08-14 21:35:54 +0800

    全局异常捕获401状态码，进行重新登录拦截

--- java-web-codes/vue-tlias-management/src/utils/request.js
+++ java-web-codes/vue-tlias-management/src/utils/request.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { ElMessage } from "element-plus";
+import router from "@/router";
 
 const request = axios.create({
   baseURL: "/api",
@@ -21,6 +23,12 @@ request.interceptors.response.use(
     return result.data;
   },
   (error) => {
+    if (error.response.status === 401) {
+      ElMessage.error('登录失效，请重新登录');
+      router.push('/login');
+    } else {
+      ElMessage.error('接口访问异常');
+    }
     return Promise.reject(error);
   },
 );
